22375f47fc93100c8eb54d7408f84a1a4603098f
Natan <natancintra28@gmail.com>
2026-06-30 16:22:06 -0300

    commit inicial

---
+++ src/main/java/com/javanauta/agendadoratarefas/AgendadoraTarefasApplication.java
@@ -0,0 +1,13 @@
+package com.javanauta.agendadoratarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendadoraTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendadoraTarefasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javanauta/agendadoratarefas/AgendadoraTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javanauta.agendadoratarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendadoraTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
